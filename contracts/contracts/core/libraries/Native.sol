137e93c17b2d77f9b6b48959454b193347415f4b
Julio M Cruz <julio.cruz@eb-ms.net>
2026-06-13 11:38:46 -0400

    add core libraries

---
+++ contracts/contracts/core/libraries/Native.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract Native {
+	/// @notice Address of the native token marker.
+	address public constant NATIVE = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+}
